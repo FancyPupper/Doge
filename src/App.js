224e0e4c43ec20a504a6feeaa1dd39d1cbdc1b10
FancyPupper <A01274757@itesm.mx>
2019-03-09 20:26:11 -0600

    Added: dynamic text top and bottom

--- src/App.js
+++ src/App.js
@@ -2,15 +2,41 @@ import React, { Component } from 'react';
 import './App.css';
 
 class DogeMeme extends Component {
-	
+	constructor(props){
+		super(props);
+		this.state = {
+			top:'',
+			bottom:''
+		};
+
+	// this.handleInputChange = this.handleInputChange.bind(this);
+
+	}
+	handleInputChange = (event) => {
+		let val = event.target.value;
+		let name = event.target.name;
+		// console.log(name);
+		// console.log(val)
+		this.setState({
+			[name]: val
+		})
+	}
+
 	render(){
 		return (
-			<div className="dog">
-				<div  id="up" className="white">
-					<p>{this.props.top}</p>
-				</div>
-				<div id="down" className="white">
-					<p>{this.props.bottom}</p>
+			<div>
+				<form id="ins">
+	        		<input type="text" name="top" onChange={this.handleInputChange}/>
+	        		<input type="text" name="bottom" onChange={this.handleInputChange}/>
+	      		</form>
+
+				<div className="dog">
+					<div  id="up" className="white">
+						<p>{this.state.top}</p>
+					</div>
+					<div id="down" className="white">
+						<p>{this.state.bottom}</p>
+					</div>
 				</div>
 			</div>
 		)
